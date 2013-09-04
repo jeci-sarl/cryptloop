b93278372539736360842a127cafe60ceb80a073
itair <jeremie.lesage@gmail.com>
2013-09-05 01:44:20 +0200

    config for ext4

--- config.sh
+++ config.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#MOUNT_OPT="-o noatime,nodiratime,nodev,exec,nosuid,owner=$(whoami),compress=lzo"
-MOUNT_OPT="-o noatime,nodiratime,nodev,exec,nosuid"
-DM_CRYPT_NAME_DEVICE=dsk1
+#MOUNT_OPT="-o noatime,nodiratime,nodev,exec,nosuid,compress=lzo"
 
+## NOT FOR EXT4
+# MOUNT_OPT="-o noatime,nodiratime,nodev,exec,nosuid,umask=0022,gid=$(id -g),uid=$(id -u)"
+##
+MOUNT_OPT="-o noatime,nodiratime,nodev,exec,nosuid"
+
+
+DM_CRYPT_NAME_DEVICE=dsk1
 
 DEBUG=true
 function debug { if [ "true" != "$DEBUG" ]; then return 1; fi }
